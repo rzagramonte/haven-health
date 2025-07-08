7ff450a766185040c6f828d32f920f556fc5deba
Noah Harris <ncharris93@gmail.com>
2025-07-08 10:36:34 -0400

    test: add vitest

---
+++ lib/config/config.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest";
+
+import { AppConfig } from "./index";
+
+describe("AppConfig", () => {
+  it("should have isDev set to false in test environment", () => {
+    expect(AppConfig.isDev).toBe(false);
+  });
+
+  it("should have isProd set to false in test environment", () => {
+    expect(AppConfig.isProd).toBe(false);
+  });
+});
