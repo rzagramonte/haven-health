f34c4424be400dea2e892eecd70d9a12bfc2a4a0
Noah Harris <ncharris93@gmail.com>
2025-07-08 16:17:55 -0400

    feat(cd): enhance CI/CD workflow with Vercel deployment steps and build process refactor(supabase): centralize Supabase configuration in AppConfig chore: add cSpell configuration for Supabase and Vercel terms delete: remove unused vercel.json configuration file

--- lib/config/index.ts
+++ lib/config/index.ts
@@ -3,4 +3,8 @@
 export const AppConfig = Object.freeze({
   isDev: process.env.NODE_ENV === 'development',
   isProd: process.env.NODE_ENV === 'production',
+  supabase: {
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    anonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
+  },
 })
