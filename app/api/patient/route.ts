e0a1dcdee795d8d80c7ec0dafc73d85334789793
Khurram Ali <65960587+destocot@users.noreply.github.com>
2025-07-19 18:44:49 -0400

    Revert "feat(app/api): created placeholder files for all api routes"

--- app/api/patient/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  return new NextResponse('Success! This route is for getting all patients')
-}
-
-export async function POST() {
-  return new NextResponse('Success! This route is for adding a new patient')
-}
