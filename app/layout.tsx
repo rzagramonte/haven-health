89e92051fa6503819295a87ab0fed3206cfb16a4
Walter Furrer <walterfurrer.dev@gmail.com>
2025-07-19 17:21:41 -0400

    Update layout.tsx
    Re-added Toaster into RootLayout return.

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster position="top-center" richColors />
         </ThemeProvider>
       </body>
     </html>
