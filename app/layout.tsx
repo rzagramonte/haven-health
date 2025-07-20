66d110bf84a8107de6cf0151cd0ed729ff4634c6
petertasca <tascapeter@gmail.com>
2025-07-20 11:57:38 -0400

    refactor(app): merged with updated main branch
    merged footer branch with updated main branch and deleted supabase test file

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 import { ThemeProvider } from 'next-themes'
 import { Toaster } from 'sonner'
 
-// import { Footer } from '@/components/ui/footer'
+import { Footer } from '@/components/ui/footer'
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -39,6 +39,7 @@ export default function RootLayout({
         >
           {children}
           <Toaster position="top-center" richColors />
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
