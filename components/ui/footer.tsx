787135128497b00f528914b3736e42d85ab6cca5
petertasca <tascapeter@gmail.com>
2025-07-20 16:49:30 -0400

    refactor(footer.tsx): updated icontype import
    added type before IconType import in footer.tsx

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { IconType } from 'react-icons'
+import type { IconType } from 'react-icons'
 import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
 
 // Type for a single link in quickLinks
