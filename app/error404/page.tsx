d750d188792af1ef8ff86a9a0e76c0752914d953
petertasca <tascapeter@gmail.com>
2025-07-21 14:41:54 -0400

    feat(app/error404): created an error404page component
    recustomized a 404 page component from tailwind and styled according to our css environment
    variables. also renamed 404 file and component to error404

--- app/error404/page.tsx
+++ app/error404/page.tsx
@@ -9,23 +9,28 @@ export default function Error404() {
         <body class="h-full">
         ```
       */}
-      <main className="grid min-h-full place-items-center bg-[--color-foreground] px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">404</p>
-          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+      <main className="grid min-h-full place-items-center bg-[var(--color-background)] px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center border-1 border-black p-[2em] bg-[var(--color-card)]">
+          <p className="text-base font-semibold text-[var(--color-accent)]">
+            404
+          </p>
+          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-[var(--color-foreground)] sm:text-7xl">
             Page not found
           </h1>
-          <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+          <p className="mt-6 text-lg font-medium text-pretty text-[var(--color-muted-foreground)] sm:text-xl/8">
             Sorry, we couldn’t find the page you’re looking for.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="#"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md bg-[var(--color-accent)] px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-[var(--color-primary)] focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Go back home
             </a>
-            <a href="#" className="text-sm font-semibold text-gray-900">
+            <a
+              href="#"
+              className="text-sm font-semibold text-[var(--color-foreground)]"
+            >
               Contact support <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
