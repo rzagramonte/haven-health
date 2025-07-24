3e273453657a8740d6d4e3762ae55e14df63f725
petertasca <tascapeter@gmail.com>
2025-07-24 15:39:12 -0400

    feat(providerprofile/editproviderprofile): editProviderProfile component
    created a client component called editProviderProfile so provider/profile/page.tsx could remain a
    server component

---
+++ components/ui/providerProfile/editableEmergencyContactField.tsx
@@ -0,0 +1,52 @@
+import { Input } from '../input'
+export type EmergencyContact = {
+  firstName: string
+  lastName: string
+  phone: string
+}
+
+export interface EmergencyContactFieldProps {
+  value: EmergencyContact
+  editing: boolean
+  onUpdate: (val: EmergencyContact) => void
+}
+
+export default function EditableEmergencyContactField({
+  value,
+  editing,
+  onUpdate,
+}: EmergencyContactFieldProps) {
+  if (!editing) {
+    return (
+      <>
+        <p>
+          {value.firstName} {value.lastName}
+        </p>
+        <p>{value.phone}</p>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <Input
+        aria-label="First Name"
+        className="bg-muted"
+        value={value.firstName}
+        onChange={(e) => onUpdate({ ...value, firstName: e.target.value })}
+      />
+      <Input
+        aria-label="Last Name"
+        className="bg-muted"
+        value={value.lastName}
+        onChange={(e) => onUpdate({ ...value, lastName: e.target.value })}
+      />
+      <Input
+        aria-label="Phone Number"
+        className="bg-muted"
+        value={value.phone}
+        onChange={(e) => onUpdate({ ...value, phone: e.target.value })}
+      />
+    </>
+  )
+}
