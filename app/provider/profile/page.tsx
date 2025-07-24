ada6b8786fb957adf91eeb0fb6a7f90c3a43b40c
petertasca <tascapeter@gmail.com>
2025-07-24 12:58:46 -0400

    feat(provider/profile): edit toggle
    edit toggle working for all fields

--- app/provider/profile/page.tsx
+++ app/provider/profile/page.tsx
@@ -25,6 +25,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
 
 export type ProviderDetails = [
   { label: string; key: 'name'; value: string; icon: IconType },
@@ -140,8 +141,6 @@ export default function ProfilePage() {
     editableValue: null,
   })
 
-  console.log(editState, editDispatch)
-
   return (
     <main className="flex-grow p-5">
       <div className="border-3 border-black w-full max-w-[110px]">
@@ -167,60 +166,202 @@ export default function ProfilePage() {
           </CardHeader>
           <CardContent className="border-1 border-black flex flex-col items-center">
             <ul className="flex flex-col gap-1">
-              {providerDummyData.map(({ key, label, value, icon: Icon }) => (
-                <li
-                  key={key}
-                  className="border-1 border-black flex items-center justify-between gap-2 p-1"
-                >
-                  <div className="flex gap-2">
-                    <div className="w-8 h-8 flex items-center justify-center">
-                      <Icon className="w-6 h-6" />
+              {editState.providerDetails.map(
+                ({ key, label, value, icon: Icon }) => (
+                  <li
+                    key={key}
+                    className="border-1 border-black flex items-center justify-between gap-2 p-1"
+                  >
+                    <div className="flex gap-2">
+                      <div className="w-8 h-8 flex items-center justify-center">
+                        <Icon className="w-6 h-6" />
+                      </div>
+                      <div className="flex flex-col">
+                        <p className="text-xs font-semibold">{label}</p>
+                        {editState.editingKey === key &&
+                          key === 'emergencyContact' &&
+                          typeof editState.editableValue === 'object' && (
+                            <>
+                              <Input
+                                className="bg-muted"
+                                value={editState.editableValue?.firstName}
+                                onChange={(e) => {
+                                  if (
+                                    typeof editState.editableValue ===
+                                      'object' &&
+                                    editState.editableValue != null
+                                  ) {
+                                    editDispatch({
+                                      type: 'UPDATE',
+                                      value: {
+                                        ...editState.editableValue,
+                                        firstName: e.target.value,
+                                      },
+                                    })
+                                  }
+                                }}
+                              />
+                              <Input
+                                className="bg-muted"
+                                value={editState.editableValue?.lastName}
+                                onChange={(e) => {
+                                  if (
+                                    typeof editState.editableValue ===
+                                      'object' &&
+                                    editState.editableValue != null
+                                  ) {
+                                    editDispatch({
+                                      type: 'UPDATE',
+                                      value: {
+                                        ...editState.editableValue,
+                                        lastName: e.target.value,
+                                      },
+                                    })
+                                  }
+                                }}
+                              />
+                              <Input
+                                className="bg-muted"
+                                value={editState.editableValue?.phone}
+                                onChange={(e) => {
+                                  if (
+                                    typeof editState.editableValue ===
+                                      'object' &&
+                                    editState.editableValue != null
+                                  ) {
+                                    editDispatch({
+                                      type: 'UPDATE',
+                                      value: {
+                                        ...editState.editableValue,
+                                        phone: e.target.value,
+                                      },
+                                    })
+                                  }
+                                }}
+                              />
+                            </>
+                          )}
+                        {editState.editingKey === key &&
+                          key === 'newPatients' &&
+                          typeof editState.editableValue === 'boolean' && (
+                            <div className="flex gap-2">
+                              <label htmlFor="yes-option">
+                                {value ? ' Yes' : 'No'}
+                              </label>
+                              <input
+                                type="radio"
+                                name="choice"
+                                value="yes"
+                                checked={
+                                  typeof editState.editableValue === 'boolean'
+                                    ? editState.editableValue
+                                    : value
+                                }
+                                id="yes-option"
+                                onChange={(e) =>
+                                  editDispatch({
+                                    type: 'UPDATE',
+                                    value: e.target.value,
+                                  })
+                                }
+                              />
+                              <label htmlFor="no-option">
+                                {value ? ' No' : 'Yes'}
+                              </label>
+                              <input
+                                type="radio"
+                                name="choice"
+                                value="no"
+                                checked={
+                                  typeof editState.editableValue === 'boolean'
+                                    ? !editState.editableValue
+                                    : !value
+                                }
+                                id="no-option"
+                                onChange={(e) =>
+                                  editDispatch({
+                                    type: 'UPDATE',
+                                    value: e.target.value,
+                                  })
+                                }
+                              />
+                            </div>
+                          )}
+                        {editState.editingKey === key &&
+                          key !== 'emergencyContact' &&
+                          key != 'newPatients' &&
+                          typeof editState.editableValue === 'string' && (
+                            <Input
+                              className="bg-muted"
+                              value={
+                                typeof editState.editableValue === 'string'
+                                  ? editState.editableValue
+                                  : value
+                              }
+                              onChange={(e) =>
+                                editDispatch({
+                                  type: 'UPDATE',
+                                  value: e.target.value,
+                                })
+                              }
+                            />
+                          )}
+                        {editState.editingKey !== key &&
+                          key === 'emergencyContact' &&
+                          typeof value === 'object' && (
+                            <>
+                              <p>
+                                {value.firstName} {value.lastName}
+                              </p>
+                              <p>{value.phone}</p>
+                            </>
+                          )}
+                        {editState.editingKey !== key &&
+                          key === 'newPatients' &&
+                          typeof value === 'boolean' && (
+                            <div className="flex gap-2">
+                              <label htmlFor="yes-option">
+                                {value ? ' Yes' : 'No'}
+                              </label>
+                              <input
+                                type="radio"
+                                name="choice"
+                                value="yes"
+                                checked={value}
+                                id="yes-option"
+                                readOnly
+                              />
+                              <label htmlFor="no-option">
+                                {value ? ' No' : 'Yes'}
+                              </label>
+                              <input
+                                type="radio"
+                                name="choice"
+                                value="no"
+                                checked={!value}
+                                id="no-option"
+                                readOnly
+                              />
+                            </div>
+                          )}
+                        {editState.editingKey !== key &&
+                          key !== 'emergencyContact' &&
+                          key !== 'newPatients' && <p>{value}</p>}
+                      </div>
                     </div>
-                    <div className="flex flex-col">
-                      <p className="text-xs font-semibold">{label}</p>
-                      {key === 'emergencyContact' &&
-                      typeof value === 'object' ? (
-                        <>
-                          <p>
-                            {value.firstName} {value.lastName}
-                          </p>
-                          <p>{value.phone}</p>
-                        </>
-                      ) : key === 'newPatients' ? (
-                        <div className="flex gap-2">
-                          <label htmlFor="yes-option">
-                            {value ? ' Yes' : 'No'}
-                          </label>
-                          <input
-                            type="radio"
-                            name="choice"
-                            value="yes"
-                            checked={value}
-                            id="yes-option"
-                          />
-                          <label htmlFor="no-option">
-                            {value ? ' No' : 'Yes'}
-                          </label>
-                          <input
-                            type="radio"
-                            name="choice"
-                            value="no"
-                            checked={!value}
-                            id="no-option"
-                          />
-                        </div>
-                      ) : (
-                        <p>{value}</p>
-                      )}
-                    </div>
-                  </div>
-                  <CardAction className="">
-                    <Button className="w-6 h-6 cursor-pointer">
-                      <FaEdit />
-                    </Button>
-                  </CardAction>
-                </li>
-              ))}
+                    <CardAction className="">
+                      <Button
+                        className="w-6 h-6 cursor-pointer"
+                        onClick={() => {
+                          editDispatch({ type: 'EDIT', key: key, value: value })
+                        }}
+                      >
+                        <FaEdit />
+                      </Button>
+                    </CardAction>
+                  </li>
+                ),
+              )}
             </ul>
           </CardContent>
           <CardFooter>
