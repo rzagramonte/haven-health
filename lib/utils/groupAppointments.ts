08e6683ac3a34882d574a3942f4eafcb8c01cf9b
Roosiel <rzagramonte@outlook.com>
2025-08-06 14:21:23 -0400

    fixed a TypeScript error in lib/utils/groupAppointments.ts at line 12: appt.appointment_time can be null, but new Date() does not accept null as an argument.

--- lib/utils/groupAppointments.ts
+++ lib/utils/groupAppointments.ts
@@ -8,29 +8,32 @@ export function groupAppointmentsByTimeAgo(
   if (!appointments) {
     throw new Error('useFormField should be used within <FormField>')
   }
+
   for (const appt of appointments) {
-    const date = new Date(appt.appointment_time)
-    const diffDays = Math.floor(
-      (now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24),
-    )
+    if (appt?.appointment_time) {
+      const date = new Date(appt.appointment_time)
+      const diffDays = Math.floor(
+        (now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24),
+      )
 
-    let bucket = ''
-    if (diffDays < 30) {
-      bucket = 'This month'
-    } else if (diffDays < 60) {
-      bucket = '1 month ago'
-    } else if (diffDays < 180) {
-      bucket = '6 months ago'
-    } else if (diffDays < 365) {
-      bucket = '1 year ago'
-    } else {
-      bucket = `${Math.floor(diffDays / 365)} years ago`
-    }
+      let bucket = ''
+      if (diffDays < 30) {
+        bucket = 'This month'
+      } else if (diffDays < 60) {
+        bucket = '1 month ago'
+      } else if (diffDays < 180) {
+        bucket = '6 months ago'
+      } else if (diffDays < 365) {
+        bucket = '1 year ago'
+      } else {
+        bucket = `${Math.floor(diffDays / 365)} years ago`
+      }
 
-    if (!groups[bucket]) {
-      groups[bucket] = []
+      if (!groups[bucket]) {
+        groups[bucket] = []
+      }
+      groups[bucket].push(appt)
     }
-    groups[bucket].push(appt)
   }
 
   return groups
