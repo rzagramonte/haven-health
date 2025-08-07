dd47e81739c11c0b8da532905a4ab5788a370627
petertasca <tascapeter@gmail.com>
2025-08-07 12:31:24 -0400

    feat(components and utils): added helper functions and Medical/Appointment Accordions
    added helper functions and Medical/Appointment Accordions

---
+++ utils/helpers.tsx
@@ -0,0 +1,29 @@
+export function formatDate(dateString: string) {
+  const date = new Date(dateString)
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+  const day = date.getDate()
+  const month = monthNames[date.getMonth()]
+  const year = date.getFullYear()
+
+  return `${day} ${month}, ${year}`
+}
+
+export const getAge = (dateOfBirth: string) => {
+  const today = new Date()
+  const birthDate = new Date(dateOfBirth)
+  const age = today.getFullYear() - birthDate.getFullYear()
+  return age
+}
