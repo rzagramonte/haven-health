b593c2c1b70f01b803f8affa33f19751226de88f
Walter Furrer <walterfurrer.dev@gmail.com>
2025-08-08 19:41:22 -0400

    feat: Provider Appointments/Schedule Page/feat: Make Footer Sticky (#70)
    * feat: add @radix-ui/react-tabs dependency and enhance user authentication queries
    
    - Added @radix-ui/react-tabs to package.json dependencies.
    - Implemented detailed logging in signUp function for better debugging.
    - Enhanced getCurrentUser function to retrieve and return user session and data.
    - Added getCurrentPerson function to fetch person details based on user ID.
    - Introduced getPatientWithPersonId function to retrieve patient data using person ID.
    - Updated getPatient function to include medical visit details in the response.
    
    * Made the footer sticky
    
    * Copied server/auth/queries.ts file from main
    
    * wip
    
    * Finished daily and weekly views for providers to view.
    
    * Added min-w to daily view
    
    * Made changes per comments
    
    * Made changes per Khurram's notes.
    
    * Deleted and re-installed hoping it fixes this new merge conflict all of the sudden.
    
    * plz fix merge conflict i beg
    
    * Here we go again....
    
    * removing and re-installing again
    
    * more merge conflicts :/
    
    * fix: test
    
    ---------
    
    Co-authored-by: Khurram Ali <khurramcali@gmail.com>

---
+++ app/provider/schedule/todaysAppointmentsTable.tsx
@@ -0,0 +1,81 @@
+import Link from 'next/link'
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { getAppointmentsByDateRange } from '@/server/appointment/queries'
+
+import { formatTime } from '../../../utils/helper'
+
+export default async function TodaysAppointmentsTable() {
+  const today = new Date()
+
+  const startOfDay = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate(),
+  )
+
+  const endOfDay = new Date(startOfDay)
+  endOfDay.setDate(startOfDay.getDate() + 1)
+
+  const appointmentData = await getAppointmentsByDateRange(startOfDay, endOfDay)
+
+  if (!appointmentData || appointmentData.length === 0) {
+    return <p className="p-4">No appointments found for today.</p>
+  }
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Time</TableHead>
+          <TableHead>Patient</TableHead>
+          <TableHead>Appointment Type</TableHead>
+          <TableHead>Doctor</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {appointmentData.map((appointment) => {
+          const apptDate = appointment.appointment_time
+            ? new Date(appointment.appointment_time)
+            : undefined
+          const hasPassed = apptDate ? apptDate < new Date() : false
+
+          return (
+            <TableRow
+              key={appointment.id}
+              className={`bg-card-1 hover:bg-black/5 ${
+                hasPassed ? 'opacity-50 line-through' : ''
+              }`}
+            >
+              <TableCell className="font-medium text-primary">
+                {appointment.appointment_time
+                  ? formatTime(appointment.appointment_time)
+                  : 'N/A'}
+              </TableCell>
+              <TableCell>
+                <Link
+                  href="#"
+                  className="font-medium underline underline-offset-2"
+                >
+                  {appointment.patient?.person?.first_name}{' '}
+                  {appointment.patient?.person?.last_name}
+                </Link>
+              </TableCell>
+              <TableCell>{appointment.appointment_type}</TableCell>
+              <TableCell>
+                {'Dr.'} {appointment.provider?.last_name}
+              </TableCell>
+            </TableRow>
+          )
+        })}
+      </TableBody>
+    </Table>
+  )
+}
