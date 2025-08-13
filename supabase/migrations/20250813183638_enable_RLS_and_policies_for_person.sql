b8a63cd6094948f9262eeb15029c4b612c47d840
petertasca <tascapeter@gmail.com>
2025-08-13 16:08:25 -0400

    feature: RSL for person table

---
+++ supabase/migrations/20250813183638_enable_RLS_and_policies_for_person.sql
@@ -0,0 +1,38 @@
+
+
+
+ALTER TABLE public.person ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Users can insert their own person record when signing up"
+ON public.person
+FOR INSERT
+TO authenticated
+WITH CHECK ((select auth.uid()) = person_uuid);
+
+CREATE POLICY "only authenticated users can see their own person data only"
+on public.person
+FOR SELECT
+TO authenticated
+USING ((select auth.uid()) = person_uuid);
+
+CREATE POLICY "only providers can see all the person data"
+on public.person
+FOR SELECT
+TO authenticated
+USING (
+    EXISTS (
+        SELECT 1
+        FROM public.person as p
+        WHERE p.person_uuid = auth.uid()
+        AND p.role = 'provider'
+    )
+);
+
+
+CREATE POLICY "Users can update their own person record"
+ON public.person
+FOR UPDATE
+TO authenticated
+USING ((select auth.uid()) = person_uuid)
+WITH CHECK ((select auth.uid()) = person_uuid);
+
