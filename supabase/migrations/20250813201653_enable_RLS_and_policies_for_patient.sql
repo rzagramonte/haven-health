711c862deb722611a345d9efaecf477f7992164e
petertasca <tascapeter@gmail.com>
2025-08-13 18:17:20 -0400

    fix: save file

---
+++ supabase/migrations/20250813201653_enable_RLS_and_policies_for_patient.sql
@@ -0,0 +1,20 @@
+ALTER TABLE public.patient ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Only authenticated users can see patient records"
+ON public.patient
+FOR SELECT
+TO authenticated
+USING (true);
+
+
+CREATE POLICY 'Only authenticated users can insert on patient records'
+ON public.patient
+FOR INSERT
+TO authenticated
+WITH CHECK (true);
+
+CREATE POLICY 'Only authenticated users can update patient records'
+ON public.patient
+FOR UPDATE
+TO authenticated
+WITH CHECK (true)
